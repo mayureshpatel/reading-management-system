e16de40f1c5de203ee39a5a1b2a778a8970f0ed8
mayureshpatel <mayureshpatel1795@gmail.com>
2022-03-12 15:15:44 -0500

    Moved code that loaded the top lists and top genres into methods of their own to discretely identify what happens when the page loads.

--- reading-management-system/Site.Master.cs
+++ reading-management-system/Site.Master.cs
@@ -21,85 +21,94 @@ namespace reading_management_system
         {
             if (!IsPostBack)
             {
-                String selectListsCommand = "SELECT TOP(5) ReadingList.ReadingListName, COUNT(ReadingListAffiliations.FK_BookID) AS BookCount" +
+                Load_Top_Lists();
+                Load_Top_Genres();
+            }
+        }
+
+        protected void Load_Top_Lists()
+        {
+            String selectListsCommand = "SELECT TOP(5) ReadingList.ReadingListName, COUNT(ReadingListAffiliations.FK_BookID) AS BookCount" +
                     " FROM ReadingList" +
                     " INNER JOIN ReadingListAffiliations ON ReadingList.ReadingListID = ReadingListAffiliations.FK_ReadingListID" +
                     " GROUP BY ReadingList.ReadingListName" +
                     " ORDER BY BookCount DESC";
 
-                using (SqlConnection myConnection = new SqlConnection(myConnectionString))
+            using (SqlConnection myConnection = new SqlConnection(myConnectionString))
+            {
+                using (SqlCommand myCommand = new SqlCommand(selectListsCommand, myConnection))
                 {
-                    using (SqlCommand myCommand = new SqlCommand(selectListsCommand, myConnection))
+                    SqlDataAdapter listsReader = new SqlDataAdapter(myCommand);
+
+                    DataSet listsDS = new DataSet();
+                    listsReader.Fill(listsDS);
+
+                    listsTable = listsDS.Tables[0].Copy();
+
+                    // Default html anchors
+                    HtmlAnchor[] defaultControls = { BrowseListItem01, BrowseListItem02, BrowseListItem03, BrowseListItem04, BrowseListItem05 };
+
+                    // If the table does not have any rows, we will populate the links with a default value
+                    if (listsTable.Rows.Count == 0)
                     {
-                        SqlDataAdapter listsReader = new SqlDataAdapter(myCommand);
-
-                        DataSet listsDS = new DataSet();
-                        listsReader.Fill(listsDS);
-
-                        listsTable = listsDS.Tables[0].Copy();
-
-                        // Default html anchors
-                        HtmlAnchor[] defaultControls = { BrowseListItem01, BrowseListItem02, BrowseListItem03, BrowseListItem04, BrowseListItem05 };
-
-                        // If the table does not have any rows, we will populate the links with a default value
-                        if (listsTable.Rows.Count == 0)
+                        for (int i = 0; i < defaultControls.Length; i++)
                         {
-                            for (int i = 0; i < defaultControls.Length; i++)
-                            {
-                                defaultControls[i].InnerHtml = "Not Enough Data";
-                            }
+                            defaultControls[i].InnerHtml = "Not Enough Data";
                         }
-                        // If the table has some rows but not 5, we will populate what we can, then populate the rest with a default value
-                        else if (listsTable.Rows.Count < 5)
+                    }
+                    // If the table has some rows but not 5, we will populate what we can, then populate the rest with a default value
+                    else if (listsTable.Rows.Count < 5)
+                    {
+                        // Add the ones that can be added
+                        int nextToAdd = 0;
+                        for (int i = 0; i < listsTable.Rows.Count; i++)
                         {
-                            // Add the ones that can be added
-                            int nextToAdd = 0;
-                            for (int i = 0; i < listsTable.Rows.Count; i++)
-                            {
-                                defaultControls[i].InnerHtml = (String)listsTable.Rows[i]["ReadingListName"];
-                                nextToAdd = i + 1;
-                            }
-
-                            // Add defaults for the others
-                            for (int i = nextToAdd; i < defaultControls.Length; i++)
-                            {
-                                defaultControls[i].InnerHtml = "Not Enough Data";
-                            }
+                            defaultControls[i].InnerHtml = (String)listsTable.Rows[i]["ReadingListName"];
+                            nextToAdd = i + 1;
                         }
-                        // If the table has 5 or more rows, just use those rows
-                        else
+
+                        // Add defaults for the others
+                        for (int i = nextToAdd; i < defaultControls.Length; i++)
                         {
-                            for (int i = 0; i < defaultControls.Length; i++)
-                            {
-                                defaultControls[i].InnerHtml = (String)listsTable.Rows[i]["ReadingListName"];
-                            }
+                            defaultControls[i].InnerHtml = "Not Enough Data";
+                        }
+                    }
+                    // If the table has 5 or more rows, just use those rows
+                    else
+                    {
+                        for (int i = 0; i < defaultControls.Length; i++)
+                        {
+                            defaultControls[i].InnerHtml = (String)listsTable.Rows[i]["ReadingListName"];
                         }
                     }
                 }
+            }
+        }
 
-                String selectGenresCommand = "SELECT Genre.GenreName, COUNT(GenreAffiliations.FK_BookID) AS BookCount" +
+        protected void Load_Top_Genres()
+        {
+            String selectGenresCommand = "SELECT Genre.GenreName, COUNT(GenreAffiliations.FK_BookID) AS BookCount" +
                     " FROM Genre" +
                     " INNER JOIN GenreAffiliations ON Genre.GenreID = GenreAffiliations.FK_GenreID" +
                     " GROUP BY Genre.GenreName HAVING(NOT (Genre.GenreName = 'Non Fiction')) AND(NOT(Genre.GenreName = 'Fiction'))" +
                     " ORDER BY BookCount DESC";
 
-                using (SqlConnection myConnection = new SqlConnection(myConnectionString))
+            using (SqlConnection myConnection = new SqlConnection(myConnectionString))
+            {
+                using (SqlCommand myCommand = new SqlCommand(selectGenresCommand, myConnection))
                 {
-                    using (SqlCommand myCommand = new SqlCommand(selectGenresCommand, myConnection))
-                    {
-                        SqlDataAdapter listsReader = new SqlDataAdapter(myCommand);
+                    SqlDataAdapter listsReader = new SqlDataAdapter(myCommand);
 
-                        DataSet listsDS = new DataSet();
-                        listsReader.Fill(listsDS);
+                    DataSet listsDS = new DataSet();
+                    listsReader.Fill(listsDS);
 
-                        genresTable = listsDS.Tables[0].Copy();
+                    genresTable = listsDS.Tables[0].Copy();
 
-                        BrowseGenreItem01.InnerHtml = (String)genresTable.Rows[0]["GenreName"];
-                        BrowseGenreItem02.InnerHtml = (String)genresTable.Rows[1]["GenreName"];
-                        BrowseGenreItem03.InnerHtml = (String)genresTable.Rows[2]["GenreName"];
-                        BrowseGenreItem04.InnerHtml = (String)genresTable.Rows[3]["GenreName"];
-                        BrowseGenreItem05.InnerHtml = (String)genresTable.Rows[4]["GenreName"];
-                    }
+                    BrowseGenreItem01.InnerHtml = (String)genresTable.Rows[0]["GenreName"];
+                    BrowseGenreItem02.InnerHtml = (String)genresTable.Rows[1]["GenreName"];
+                    BrowseGenreItem03.InnerHtml = (String)genresTable.Rows[2]["GenreName"];
+                    BrowseGenreItem04.InnerHtml = (String)genresTable.Rows[3]["GenreName"];
+                    BrowseGenreItem05.InnerHtml = (String)genresTable.Rows[4]["GenreName"];
                 }
             }
         }
